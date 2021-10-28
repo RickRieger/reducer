7d4dc9b4a2582f5b2546da9f077a27af7cf1a7fd
Rick Rieger <rickrieger36251@gmail.com>
2021-10-28 13:48:22 -0400

    initial

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import CounterOne from './components/CounterOne';
+function App() {
+  return (
+    <div className='App'>
+      <CounterOne />
+    </div>
+  );
+}
+
+export default App;
